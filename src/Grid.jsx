7153f797091a3de285c1083d92518849dd86ffa0
Jason Saelhof <jsaelhof@gmail.com>
2022-01-29 22:34:44 -0700

    fix top margin issue with background

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -3,8 +3,7 @@ const Grid = ({ children }) => (
     style={{
       display: "grid",
       columnGap: 24,
-      marginTop: 24,
-      marginBottom: 32,
+      padding: "12px 0",
       gridTemplateColumns: "80px repeat(10, 120px)",
       gridTemplateRow: "max-content",
       gridAutoFlow: "row",
